4993713e22b30c70d7dd780f4ef81aa249797bc6
Lawyer0901 <lawyer0901@gmail.com>
2023-11-06 20:48:37 +0200

    f

--- src/components/Title.styled.jsx
+++ src/components/Title.styled.jsx
@@ -2,11 +2,10 @@ import styled from 'styled-components';
 
 export const Title = styled.h1`
   margin-bottom: 1em;
-  text-shadow: 1px 1px 0 hsl(20, 100%, 50%), 2px 2px 0 hsl(20, 100%, 50%),
-    3px 3px 0 hsl(35, 100%, 50%), 4px 4px 0 hsl(35, 100%, 50%),
-    5px 5px 0 hsl(45, 100%, 50%), 6px 6px 0 hsl(45, 100%, 55%),
-    7px 7px 0 hsl(55, 100%, 60%), 8px 8px 0 hsl(55, 100%, 65%);
-  color: hsl(0, 100%, 40%);
+  color: rgba(0, 0, 0, 0.6);
+
+  text-shadow: 2px 8px 6px rgba(0, 0, 0, 0.2),
+    0px -5px 35px rgba(255, 255, 255, 0.3);
   transition: all 1s;
   text-align: center;
 
@@ -21,11 +20,10 @@ export const Title = styled.h1`
 
 export const Contacts = styled.h2`
   margin-top: 1em;
-  text-shadow: 1px 1px 0 hsl(20, 100%, 50%), 2px 2px 0 hsl(20, 100%, 50%),
-    3px 3px 0 hsl(35, 100%, 50%), 4px 4px 0 hsl(35, 100%, 50%),
-    5px 5px 0 hsl(45, 100%, 50%), 6px 6px 0 hsl(45, 100%, 55%),
-    7px 7px 0 hsl(55, 100%, 60%), 8px 8px 0 hsl(55, 100%, 65%);
-  color: hsl(0, 100%, 40%);
+  color: rgba(0, 0, 0, 0.6);
+
+  text-shadow: 2px 8px 6px rgba(0, 0, 0, 0.2),
+    0px -5px 35px rgba(255, 255, 255, 0.3);
   transition: all 1s;
   text-align: center;
 
