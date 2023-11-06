2b8adb847dcad6c487bd4554363e3f9dad4b0c41
Lawyer0901 <lawyer0901@gmail.com>
2023-11-06 20:51:30 +0200

    g

--- src/components/Title.styled.jsx
+++ src/components/Title.styled.jsx
@@ -10,10 +10,8 @@ export const Title = styled.h1`
   text-align: center;
 
   &:hover {
-    text-shadow: 1px -1px 0 hsl(290, 100%, 40%), 2px -2px 0 hsl(290, 100%, 40%),
-      3px -3px 0 hsl(280, 100%, 60%), 4px -4px 0 hsl(280, 100%, 60%),
-      5px -5px 0 hsl(270, 100%, 75%), 6px -6px 0 hsl(270, 100%, 80%),
-      7px -7px 0 hsl(260, 100%, 85%), 8px -8px 0 hsl(260, 100%, 90%);
+    text-shadow: 1px 1px white, 2px 2px tomato;
+    color: crimson;
     color: hsl(300, 100%, 30%);
   }
 `;
@@ -28,10 +26,8 @@ export const Contacts = styled.h2`
   text-align: center;
 
   &:hover {
-    text-shadow: 1px -1px 0 hsl(290, 100%, 40%), 2px -2px 0 hsl(290, 100%, 40%),
-      3px -3px 0 hsl(280, 100%, 60%), 4px -4px 0 hsl(280, 100%, 60%),
-      5px -5px 0 hsl(270, 100%, 75%), 6px -6px 0 hsl(270, 100%, 80%),
-      7px -7px 0 hsl(260, 100%, 85%), 8px -8px 0 hsl(260, 100%, 90%);
+    text-shadow: 1px 1px white, 2px 2px tomato;
+    color: crimson;
     color: hsl(300, 100%, 30%);
   }
 `;
